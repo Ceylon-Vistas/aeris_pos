202812c05069d1984fe5453cc6bc11dd93860239
Ceylon-Vistas <kavithmaceylonvistas@gmail.com>
2026-08-08 17:41:53 +0530

    Initial commit

---
+++ backend/src/test/java/aeris_island/backend/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package aeris_island.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

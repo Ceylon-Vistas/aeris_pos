f089230ae1647a39ee8e16dd4369153f7989f1c8
Ceylon-Vistas <kavithmaceylonvistas@gmail.com>
2026-08-09 17:24:39 +0530

    updated 2

---
+++ electron.ts
@@ -0,0 +1,29 @@
+import {app, BrowserWindow} from "electron";
+import path from "path";
+
+function createWindow(): void {
+    const win = new BrowserWindow({
+        width: 1600,
+        height: 800,
+        webPreferences: {
+            contextIsolation: true,
+            nodeIntegration: false
+        }
+    });
+    win.loadFile(path.join(__dirname, "../index.html"));
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
